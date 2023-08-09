d2a6f6dda55c0adb6866e890c8963cb6bdc489ed
Matthew Espinoza <70907264+MattEspoz@users.noreply.github.com>
2023-08-08 21:48:14 -0400

    added better project view

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -10,12 +10,14 @@ import {
 } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
+import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 
 interface ProjectCardProps {
   title: string;
   description: string;
   imageUrl: string;
   demoUrl: string;
+  githubUrl?: string; // Optional Github URL prop
   tag: string;
 }
 
@@ -24,13 +26,45 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   description,
   imageUrl,
   demoUrl,
+  githubUrl,
   tag,
 }) => {
-  const isFeatured = tag === "Featured";
+  const tagColors: { [key: string]: string } = {
+    "Personal Project": "linear-gradient(to right, #9EFF9C, #9AFEDC)",
+    Stealth: "linear-gradient(to right, #D3A4FF, #F2A5FD)",
+    Featured: "linear-gradient(to right, #FED79C, #FDEBA8)",
+  };
+
+  const tagTextColor: { [key: string]: string } = {
+    "Personal Project": "#056602",
+    Stealth: "#390B66",
+    Featured: "#653E02",
+  };
+
+  const backgroundGradient = tagColors[tag] || "transparent";
+  const textColor = tagTextColor[tag] || "black";
 
   return (
     <Card width="100%" borderRadius="lg" overflow="hidden">
       <Box position="relative">
+        {/* Tag on top corner */}
+        <Text
+          position="absolute"
+          top="2"
+          right="2"
+          fontSize="xs"
+          color={textColor}
+          bg={backgroundGradient}
+          borderRadius="md"
+          px={2}
+          py={1}
+          fontWeight="bold"
+          zIndex="1"
+        >
+          {tag}
+        </Text>
+
+        {/* Demo Image */}
         <Link href={demoUrl} passHref>
           <LinkOverlay>
             <div
@@ -57,7 +91,8 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
         <Text fontSize="sm" color="gray.600" mb={3}>
           {description}
         </Text>
-        <Flex justify="space-between" align="center">
+        <Flex justify="flex-start" align="center">
+          {/* View Demo Button with External Link Icon */}
           <Link href={demoUrl} passHref>
             <LinkOverlay>
               <Button
@@ -65,26 +100,30 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
                 colorScheme="purple"
                 bg="purple.300"
                 _hover={{ bg: "purple.500", color: "white" }}
+                leftIcon={<FaExternalLinkAlt />} // External link icon
+                mr={2}
               >
                 View Demo
               </Button>
             </LinkOverlay>
           </Link>
-          <Text
-            fontSize="xs"
-            color={isFeatured ? "#653E02" : "gray.500"}
-            bg={
-              isFeatured
-                ? "linear-gradient(to right, #FED79C, #FDEBA8)"
-                : "transparent"
-            }
-            borderRadius="md"
-            px={2}
-            py={1}
-            fontWeight="bold"
-          >
-            {tag}
-          </Text>
+
+          {/* Github Button with Github Icon */}
+          {githubUrl && (
+            <Link href={githubUrl} passHref>
+              <LinkOverlay>
+                <Button
+                  size="xs"
+                  colorScheme="white"
+                  bg="#475668"
+                  _hover={{ bg: "#2D3848", color: "white" }}
+                  leftIcon={<FaGithub />} // Github icon
+                >
+                  Github
+                </Button>
+              </LinkOverlay>
+            </Link>
+          )}
         </Flex>
       </CardBody>
     </Card>
