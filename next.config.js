9292b309286c88921eb791aa450d8457076da8ca
Matthew Espinoza <70907264+MattEspoz@users.noreply.github.com>
2023-08-17 15:59:37 -0400

    added 404 page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/tracker",
+        destination: "https://compound.super.site/",
+        permanent: true,
+      },
+      {
+        source: "/newsletter",
+        destination: "https://compound.beehiiv.com/",
+        permanent: true, // Set this to false if you want a temporary redirect
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
